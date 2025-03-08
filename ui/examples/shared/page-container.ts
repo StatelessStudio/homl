46b9b7a883ac9df9c0359beba8c7960d62413c1d
Drew Immerman <drewimmerman@gmail.com>
2025-03-07 19:35:02 -0500

    Move page container to scss

--- ui/examples/shared/page-container.ts
+++ ui/examples/shared/page-container.ts
@@ -1,13 +1,10 @@
+import { StyleClassesAttribute } from '../../ui-lib/dom/attribute/attributes/style-classes';
 import { px, Styling } from '../../ui-lib/dom/style';
 import { DivTag } from '../../ui-lib/dom/tags/div';
 
 export class PageContainer extends DivTag {
-	public override defaultStyling(): Styling {
-		return {
-			...super.defaultStyling(),
-			margin: 'auto',
-			maxWidth: px(800),
-			padding: '20px',
-		};
-	}
+	public override styleClasses = new StyleClassesAttribute({
+		name: 'classList',
+		value: ['page-container'],
+	});
 }
